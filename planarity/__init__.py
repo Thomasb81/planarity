3720f1480a40f6fbd85ccc98bc5d31318f214088
Thomas <thomas.burg@gmail.com>
2023-09-02 17:57:05 +0200

    Fix workflow

--- planarity/__init__.py
+++ planarity/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-#from .planarity import PGraph
+from .planarity import *
 from .planarity_functions import *
 from .planarity_networkx import *
-from .tests.test import run as test
+#from .tests.test import run as test
