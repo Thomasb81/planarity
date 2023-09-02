0f84fe4e91397078a6a775e58b133e816e4f05c3
Thomas <thomas.burg@gmail.com>
2023-09-02 18:55:55 +0200

    Fix workflow

--- debug.py
+++ debug.py
@@ -2,5 +2,6 @@ import sys
 import planarity
 sys.path = sys.path[1:]
 print(sys.path)
+print(planarity)
 print(dir(planarity))
 
--- src/planarity/__init__.py
+++ src/planarity/__init__.py
@@ -1,5 +1,3 @@
 from __future__ import absolute_import
-from . import *
 from .planarity_functions import *
 from .planarity_networkx import *
-# from .tests.test import run as test
